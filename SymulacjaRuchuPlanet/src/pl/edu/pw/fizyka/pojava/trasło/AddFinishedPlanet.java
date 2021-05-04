4306fb2d70e7e971923805044baedde2b5e75570
Adrian <Adrian@LAPTOP-Q8NT9EPV>
2021-05-04 20:59:54 +0200

    Druga Czesc projektu

--- SymulacjaRuchuPlanet/src/pl/edu/pw/fizyka/pojava/trasło/AddFinishedPlanet.java
+++ SymulacjaRuchuPlanet/src/pl/edu/pw/fizyka/pojava/trasło/AddFinishedPlanet.java
@@ -4,7 +4,14 @@ package pl.edu.pw.fizyka.pojava.tras³o;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import pl.edu.pw.fizyka.pojava.tras³o.MakePlanet.ButtonReturnListener;
@@ -13,10 +20,14 @@ public class AddFinishedPlanet extends JFrame {
 
 	JButton buttonCancel, buttonAddPlanet, buttonReturn;
 	JComboBox<String> listPlanets;
-	JPanel panelMain, panelTop1, panelTop2, panelPicture, panelList, panelListAndPicture, panelBottom;
+	JPanel panelMain, panelTop1, panelTop2, panelList, panelListAndPicture, panelBottom;
+	PicturesPlanets panelPicture; 
 	JLabel labelAddFinishedPlanet, labelList;
-	JTextField textFieldDescription;
+	JTextArea textArea;
 	String planets[]= {"Merkury","Wenus","Ziemia","Mars","Jowisz","Saturn","Uran","Neptun"};
+	List<Planet> planet = new ArrayList<Planet>();
+	
+	int x0=805,y0=790;
 	
 	public AddFinishedPlanet() throws HeadlessException {
 		super(" Symulacja ruchu planetarnego ");
@@ -52,23 +63,93 @@ public class AddFinishedPlanet extends JFrame {
 		panelList.setLayout(new BoxLayout(panelList, BoxLayout.Y_AXIS));
 		panelList.setBorder(BorderFactory.createEmptyBorder(10, 40, 10, 40));
 		labelList = new JLabel("Wybierz z listy");
-		listPlanets = new JComboBox<String>(planets);
+		listPlanets = new JComboBox<String>(planets);		
 		panelList.add(labelList);
 		panelList.add(listPlanets);
 		panelList.add(Box.createRigidArea(new Dimension(0,200)));
 		panelListAndPicture.add(panelList);
 		
-		panelPicture = new JPanel();
+		panelPicture = new PicturesPlanets();
 		panelPicture.setPreferredSize(new Dimension(30,30));
 		panelPicture.setBorder(BorderFactory.createLoweredBevelBorder());
 		panelListAndPicture.add(panelPicture);
 		panelMain.add(panelListAndPicture);
+		//listener do listy
+		listPlanets.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent arg0){
+	        	BufferedImage image = null;
+	        	URL resource = null;
+	        	if(listPlanets.getSelectedIndex()==0) {
+	        		resource = getClass().getResource("obrazki/merkury.png"); 
+	        		textArea.setText("Czeœæ jestem Merkury! Jestem najmniejsz¹ i najbli¿ej S³oñca po³o¿on¹ planet¹! \n"
+	        				+ "Niestety jestem trudny do obserwacji, ale mimo tego widaæ mnie go³ym okiem! \n"
+	        				+ "Wa¿ê 3,3*10^23 kg! Mój promieñ wynosi 2439,7 km!  "
+	        				+ "Mój obieg wokó³ S³oñca trwa 88 dni!");
+	        	}
+	        	if(listPlanets.getSelectedIndex()==1) {
+	        		resource = getClass().getResource("obrazki/wenus.png"); 
+	        		textArea.setText("Czeœæ jestem Wenus! Jestem drug¹ wed³ug oddalenia od S³oñca planet¹ Uk³adu S³onecznego! \n" 
+	        				+"Nazywaj¹ mnie tak¿e Jutrzenk¹! Jestem najjaœniejszym cia³em niebieskim zaraz po S³oñcu i Ksiê¿ycu! \n" 
+	        				+"Wa¿ê 4,9*10^24 kg!  Mój promieñ wynosi 6051,8 km.  Mój obieg wokó³ S³oñca trwa 225 dni!");
+	        	}
+	        	if(listPlanets.getSelectedIndex()==2) {
+	        		resource = getClass().getResource("obrazki/ziemia.gif"); 
+	        		textArea.setText("Czeœæ jestem Ziemia! Jestem trzeci¹ wed³ug oddalenia od S³oñca planet¹ Uk³adu S³onecznego!\n"
+	        				+"Jestem jedynym znanym miejscem we Wszechœwiecie, gdzie wystêpuje ¿ycie! \n"
+	        				+"Jestem domem dla milionów gatunków, w tym nawet dla cz³owieka! \n"
+	        				+"Obiegam S³oñce w ci¹gu 365 dni. Wiesz, ¿e wa¿ê 5,97*10^24 kg, a mój promieñ ma d³ugoœæ 6371 km? ");
+	        	}
+	        	if(listPlanets.getSelectedIndex()==3) {
+	        		resource = getClass().getResource("obrazki/mars.png"); 
+	        		textArea.setText("Witaj, jestem Mars! Jestem czwart¹ w kolejnoœci planet¹ Uk³adu S³onecznego. \n"
+	        				+ "Nazwali mnie od imienia rzymskiego boga wojny Marsa. Jestem rdzawo-czerwony z powodu tlenków ¿elaza, "
+	        				+ "które pokrywaj¹ moj¹ powierzchniê! \nObiegam S³oñce w ci¹gu 2 lat! "
+	        				+ "Wa¿ê 6,4*10^23 kg! Mój promieñ wynosi  3389,5 km !");
+	        	}
+	        	if(listPlanets.getSelectedIndex()==4) {
+	        		resource = getClass().getResource("obrazki/jowisz.png"); 
+	        		textArea.setText("Hej, jestem Jowisz! Jestem pi¹t¹ wed³ug oddalenia od S³oñca planet¹ Uk³adu S³onecznego!\n"
+	        				+ "Jestem najwiêkszy i najciê¿szy z moich przyjació³. Wa¿ê 1,9*10^27 kg.\n"
+	        				+ "Mój promieñ osi¹ga d³ugoœæ 69 911 km! Wiesz, ¿e obieg S³oñca zajmuje mi a¿ 12 lat? ");
+	        	}
+	        	if(listPlanets.getSelectedIndex()==5) {
+	        		resource = getClass().getResource("obrazki/saturn.png"); 
+	        		textArea.setText("Czeœæ jestem Saturn! Jestem szóst¹ w kolejnoœci planet¹ Uk³adu S³onecznego.\n"
+	        				+ "Zaraz po Jowiszu jestem drug¹ w kolejnoœci planet¹ pod wzglêdem wielkoœci oraz masy! \n"
+	        				+ "Moj¹ charakterystyczn¹ cech¹ s¹ pierœcienie sk³adaj¹ce siê g³ównie z lodu! \n"
+	        				+ "Wa¿ê 5,7*10^26 kg! Mój promieñ  wynosi 58 232 km! Obiegam S³oñce w ci¹gu 30 lat!!");
+	        	}
+	        	if(listPlanets.getSelectedIndex()==6) {
+	        		resource = getClass().getResource("obrazki/uran.png"); 
+	        		textArea.setText("Witaj! Jestem Uran! Jestem siódm¹ planet¹ w Uk³adzie S³onecznym. \n"
+	        				+ "Jestem trzeci¹ pod wzglêdem wielkoœci i czwart¹ pod wzglêdem masy planet¹! \n"
+	        				+ "Wiedzia³eœ, ¿e nazwali mnie na czeœæ Uranosa, greckiego boga? Wa¿ê 8,7*10^25 kg. \n"
+	        				+ "Mój promieñ wynosi 25 362 km. Obiegam S³oñce w czasie 84 lat!");
+	        	}
+	        	if(listPlanets.getSelectedIndex()==7) {
+	        		resource = getClass().getResource("obrazki/neptun.png"); 
+	        		textArea.setText("Czeœæ! Jestem Neptun! Jestem ósm¹ planet¹ w Uk³adzie S³onecznym. \n"
+	        				+ "Niestety jestem po³o¿ony najdalej od S³oñca. Jestem czwart¹ pod wzglêdem œrednicy i trzeci¹ pod wzglêdem masy planet¹! \n"
+	        				+ "Wiedzia³eœ, ¿e moja nazwa pochodzi od rzymskiego boga mórz Neptuna? Wa¿ê 1,02*10^26 kg. \n"
+	        				+ "Mój promieñ wynosi 24 622 km. Wiesz, ¿e mój obieg wokó³ S³oñca zajmujê 165 lat? ");
+	        	}
+	        	try {
+		            image = ImageIO.read(resource);
+		        } catch (IOException e) {
+		            System.err.println("Blad odczytu obrazka");
+		            e.printStackTrace();
+		        }
+	        	panelPicture.setBackgroundImage(image);
+	        }
+	      });
+
 		
 		//Opis planety
 		panelMain.add(Box.createRigidArea(new Dimension(0,20)));
-		textFieldDescription = new JTextField("Charakterystyka planety");
-		textFieldDescription.setBorder(BorderFactory.createMatteBorder(0,0,5,0,Color.blue));
-		panelMain.add(textFieldDescription);
+		textArea = new JTextArea("Charakterystyka planety");
+		textArea.setBorder(BorderFactory.createMatteBorder(0,0,5,0,Color.blue));
+		panelMain.add(textArea);
+
 		
 		//panel panelBottom
 		panelBottom = new JPanel();
@@ -78,7 +159,95 @@ public class AddFinishedPlanet extends JFrame {
 		panelBottom.add(buttonCancel);
 		panelBottom.add(buttonAddPlanet);
 		this.add(panelBottom,BorderLayout.SOUTH);
+		
+		//listener dodaj¹cy planety
+		buttonAddPlanet.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) {
+				BufferedImage image = null;
+				int o=0, t=0, x=0, y=0, r=0, r0=2, o0=4;
+	        	URL resource = null;
+				if(listPlanets.getSelectedIndex()==0) {
+	        		resource = getClass().getResource("obrazki/merkury.png");
+	        		r = r0;
+	        		o = o0+10;
+	        		x=x0/2+o;
+	        		y=y0/2+o;
+	        	}
+	        	if(listPlanets.getSelectedIndex()==1) {
+	        		resource = getClass().getResource("obrazki/wenus.png"); 
+	        		r=(int) (r0*6051.8/2439.7);
+	        		o = (int) (o0*108.141/57.740)+10;
+	        		x=x0/2+o;
+	        		y=y0/2+o;
+	        	}
+	        	if(listPlanets.getSelectedIndex()==2) {
+	        		resource = getClass().getResource("obrazki/ziemia.gif");
+	        		r=(int) (r0*6371/2439.7);
+	        		o =(int) (o0*149.504/57.740)+10;
+	        		x=x0/2+o;
+	        		y=y0/2+o;
+	        	}
+	        	if(listPlanets.getSelectedIndex()==3) {
+	        		resource = getClass().getResource("obrazki/mars.png"); 
+	        		r=(int) (r0*3389.5/2439.7);
+	        		o = (int) (o0*227.798/57.740)+10;
+	        		x=x0/2+o;
+	        		y=y0/2+o;
+	        	}
+	        	if(listPlanets.getSelectedIndex()==4) {
+	        		resource = getClass().getResource("obrazki/jowisz.png"); 
+	        		r=(int) (r0*69911/2439.7);
+	        		o = (int) (o0*777.840/57.740)+10;
+	        		x=x0/2+o;
+	        		y=y0/2+o;
+	        	}
+	        	if(listPlanets.getSelectedIndex()==5) {
+	        		resource = getClass().getResource("obrazki/saturn.png"); 
+	        		r=(int) (r0*58232/2439.7);
+	        		o = (int) (o0*1426.1/57.740)+10;
+	        		x=x0/2+o;
+	        		y=y0/2+o;
+	        	}
+	        	if(listPlanets.getSelectedIndex()==6) {
+	        		resource = getClass().getResource("obrazki/uran.png");
+	        		r=(int) (r0*25362/2439.7);
+	        		o = (int) (o0*2867.8/57.740)+10;
+	        		x=x0/2+o;
+	        		y=y0/2+o;
+	        	}
+	        	if(listPlanets.getSelectedIndex()==7) {
+	        		resource = getClass().getResource("obrazki/neptun.png"); 
+	        		r=(int) (r0*24622/2439.7);
+	        		o = (int) (o0*4493.6/57.740)+10;
+	        		x=x0/2+o;
+	        		y=y0/2+o;
+	        	}
+	        	try {
+		            image = ImageIO.read(resource);
+		        } catch (IOException e) {
+		            System.err.println("Blad odczytu obrazka");
+		            e.printStackTrace();
+		        }
+	        	
+	        	Planet pl = new Planet();
+	    		pl.setRadius(r);
+	    		pl.setX(x);
+	    		pl.setY(y);
+	    		pl.addImage(image);
+	    		pl.setPeriod(t);
+	    		
+	    		planet.add(pl);
+			}
+		});
 	}
+	public void setX0Y0(int xx0,int yy0) {
+		x0=xx0;
+		y0=yy0;
+	}
+	public List<Planet> getPlanets() {
+		return planet;
+	}
+	
 	public class ButtonReturnListener implements ActionListener{
 
 		@Override
@@ -87,3 +256,4 @@ public class AddFinishedPlanet extends JFrame {
 		}
 	}
 }
+
--- SymulacjaRuchuPlanet/src/pl/edu/pw/fizyka/pojava/trasło/PlanetsExperiment.java
+++ SymulacjaRuchuPlanet/src/pl/edu/pw/fizyka/pojava/trasło/PlanetsExperiment.java
@@ -7,8 +7,16 @@ import java.awt.GridLayout;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -16,26 +24,32 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
 import java.awt.FlowLayout;
+import java.awt.Graphics2D;
 
 public class PlanetsExperiment extends JFrame {
 	
 	JPanel p1;
-	JPanel p2;
+	AnimationPanel p2;
 	JPanel p3;
+	AddFinishedPlanet frame;
 	
 	Object selectedValue;
 	Color wybranyKolor;
-
+	
+	List<Planet> planets = new ArrayList<Planet>();
+	
 	public PlanetsExperiment() throws HeadlessException {
 		
 		super(" Symulacja ruchu planetarnego ");
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		this.setSize(900,600);
- 		this.setLocation(350,100);
+		this.setSize(1150,800);
+ 		this.setLocation(200,0);
  		
 		JPanel p1 = new JPanel();
-		JPanel p2 = new JPanel();
+		AnimationPanel p2 = new AnimationPanel();
 		JPanel p3 = new JPanel();
 		
 		add(BorderLayout.WEST, p1);
@@ -74,13 +88,24 @@ public class PlanetsExperiment extends JFrame {
 					frame.setVisible(true);
 				}
 				else if(selectedValue==wartosciWyboru[1]) {
-					AddFinishedPlanet frame = new AddFinishedPlanet();
+					frame = new AddFinishedPlanet();
 					frame.setVisible(true);
+					frame.setX0Y0(p2.width(), p2.height());
 				}
 			}
 		 };
 		 exitButton2.addActionListener(exitListener2);
+		 JButton addSelectedPlanets = new JButton ("Rysuj wybrane planety");
+		 addSelectedPlanets.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				for(Planet pl: frame.getPlanets()) {
+					planets.add(pl);
+				}
+				p2.addPlanet(planets);
+			}
+		 });
 		 p3.add(exitButton2,BorderLayout.NORTH);
+		 p3.add(addSelectedPlanets);
 		 p3.add(new JLabel());
 		 p3.add(new JLabel());
 		 p3.add(new JLabel());
@@ -100,7 +125,7 @@ public class PlanetsExperiment extends JFrame {
 		 ActionListener exitListener4 = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+				p2.start();
 			}
 		 };
 		 exitButton4.addActionListener(exitListener4);
@@ -122,13 +147,42 @@ public class PlanetsExperiment extends JFrame {
 		 nowy.addActionListener(new ActionListener(){
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
-					
+					planets.clear();
+					p2.addPlanet(planets);
 				}	
 			});
 		 save.addActionListener(new ActionListener(){
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
-					
+					JFileChooser chooser = new JFileChooser();
+					chooser.setDialogTitle("Zapisz plik");
+
+					int result = chooser.showDialog(null, "Zapisz jako");
+					@SuppressWarnings("unused")
+					File currentFile;
+					if (JFileChooser.APPROVE_OPTION == result) {
+
+					if (result != JFileChooser.APPROVE_OPTION) {
+							return;
+							}
+					try {
+						currentFile = chooser.getSelectedFile();
+						String fileName = chooser.getSelectedFile().getAbsolutePath();
+								if (!fileName.endsWith(".png")) {
+										fileName += ".png";
+										currentFile = new File(fileName);
+									}
+									BufferedImage image = new BufferedImage(p2.getWidth(), p2.getHeight(), BufferedImage.TYPE_INT_ARGB);
+									Graphics2D g2d = image.createGraphics();
+									p2.paintAll(g2d);
+									File outputfile = new File(fileName);
+								    ImageIO.write(image, "png", outputfile);
+										
+					} 
+					catch (IOException e1) {
+						System.out.println(e1.getMessage());
+					}
+					}	
 				}	
 			});
 		
@@ -158,7 +212,11 @@ public class PlanetsExperiment extends JFrame {
 		super(title, gc);
 	}
 	public static void main(String[] args) {
-		PlanetsExperiment frame = new PlanetsExperiment();
-		frame.setVisible(true);
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				PlanetsExperiment frame = new PlanetsExperiment();
+				frame.setVisible(true);
+			}
+		});
 	}
 }
